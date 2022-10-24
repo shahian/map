2e457908e05d7e629f8c15f0fa716ea7fdccb05f
h.shahian <h.shahian@haytech.ir>
2022-10-24 10:56:04 +0330

    linkedhashmap add

--- src/com/shahian/Main.java
+++ src/com/shahian/Main.java
@@ -1,6 +1,7 @@
 package com.shahian;
 
 import java.util.*;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class Main {
@@ -8,34 +9,75 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         List<Book> books = new ArrayList<>();
-        books.add(new Book(1L, "test1"));
-        books.add(new Book(2L, "test2"));
+        books.add(new Book(5L, "test1"));
+        books.add(new Book(4L, "test2"));
         books.add(new Book(3L, "test3"));
-        books.add(new Book(4L, "test4"));
-        
-        approch1(books);
-        approch2(books);
+        books.add(new Book(2L, "test4"));
+        books.add(new Book(1L, "test1"));
+
+        listToMap(books);
+        listToMapWithoutDupKey(books);
+        listToMapWithDupKey();
+        hashTable(books);
+        treemap(books);
+        listToLinkedHashMap(books);
+        linkedHashMapToList(books);
+
+    }
+
+    private static void linkedHashMapToList(List<Book> books) {
+        List<Book> books1 = new ArrayList<>();
+
+        LinkedHashMap<Long, String> hashMap = (LinkedHashMap<Long, String>) books.stream().collect(Collectors.toMap(
+                Book::getId, Book::getName, (s, s2) -> s2, LinkedHashMap::new));
+        for (Map.Entry<Long, String> longStringEntry : hashMap.entrySet()) {
+            books1.add(new Book(longStringEntry.getKey(), longStringEntry.getValue()));
+        }
+        System.out.println(books1);
 
 
     }
 
-    private static void approch2(List<Book> books) {
-        Map<Long, String> map =
-                books.stream().collect(
-                        Collectors.toMap(
+    private static void listToLinkedHashMap(List<Book> books) {
+        LinkedHashMap<Long, String> hashMap = (LinkedHashMap<Long, String>) books.stream().collect(Collectors.toMap(
+                Book::getId, Book::getName, (s, s2) -> s2, LinkedHashMap::new));
+        System.out.println(hashMap);
+    }
+
+    private static void treemap(List<Book> books) {
+        TreeMap<Long, String> map =
+                books.stream()
+                        .collect(Collectors.toMap(
                                 Book::getId,
                                 Book::getName,
-                                (x, y) -> x + " " + y
+                                (x, y) -> x + " " + y,
+                                TreeMap::new
                         ));
-
-        // print map
         map.forEach(
-                (x, y) -> System.out.println(x + "=" + y));
-
+                (x, y) -> System.out.println("treemap : " + x + " " + y)
+        );
     }
 
+    private static void hashTable(List<Book> books) {
+        Hashtable<Long, String> hashtable =
+                books.stream().collect(Collectors.toMap(
+                        Book::getId,
+                        Book::getName,
+                        (x, y)
+                                -> x + " " + y,
+                        Hashtable::new)
+                );
+        hashtable.forEach(
+                (x, y) -> System.out.println("List To HashTable : " + x + " " + y)
 
-    private static void approch1(List<Book> books) {
+        );
+
+        for (Map.Entry<Long, String> hashtable1 : hashtable.entrySet()) {
+            System.out.println("another travers  To HashTable: " + hashtable1.getKey() + " " + hashtable1.getValue());
+        }
+    }
+
+    private static void listToMap(List<Book> books) {
         Map<Long, String> map = new HashMap<>();
         for (Book book : books) {
             map.put(book.getId(), book.getName());
@@ -46,6 +88,39 @@ public class Main {
         map.entrySet().forEach(entry -> {
             System.out.println("print map details : " + entry.getKey() + " " + entry.getValue());
         });
+    }
+
+    private static void listToMapWithoutDupKey(List<Book> books) {
+        Map<Long, String> map =
+                books.stream().collect(
+                        Collectors.toMap(
+                                Book::getId,
+                                Book::getName
+                        ));
+        map.forEach(
+                (x, y) -> System.out.println("listToMapWithoutDupKey : " + x + "=" + y));
+    }
+
+    private static void listToMapWithDupKey() {
+        List<Book> books = new ArrayList<>();
+        books.add(new Book(1L, "test1"));
+        books.add(new Book(2L, "test2"));
+        books.add(new Book(2L, "test22"));
+        books.add(new Book(3L, "test3"));
+        books.add(new Book(4L, "test4"));
+        books.add(new Book(4L, "test14"));
+        Map<Long, String> map =
+                books.stream().collect(
+                        Collectors.toMap(
+                                Book::getId,
+                                Book::getName,
+                                (x, y) -> x + " duplicate " + y
+                        ));
+
+        // print map
+        map.forEach(
+                (x, y) -> System.out.println(x + "=" + y));
+
     }
 
 
